1f9f0ed921c8e9f1845471298e72fd7694ff4fd9
urlea.cristian <urlea.cristian@gmail.com>
2012-02-21 22:44:55 +0000

    guids on rooms and floors

--- witb/bob/models.py
+++ witb/bob/models.py
@@ -7,9 +7,15 @@ class Floor(models.Model):
 	date_updated = models.DateTimeField(null=True, editable=False, auto_now=True)
 	level = models.IntegerField()
 	rating = models.IntegerField(default=0, null=True)
+	guid = models.CharField(max_length=36)
 
 	def __unicode__(self):
 		return "ID#" + str(self.floor_id)+ ", date_created=" + str(self.date_created) + ", level=" + str(self.level) + ", rating=" + str(self.rating)
+
+	def save(self):
+		if not self.guid:
+			self.guid=uuid.uuid4()
+			super(Floor, self).save()
 
 class Comment(models.Model):
 	comment_id = models.AutoField(primary_key=True)
@@ -25,10 +31,7 @@ class Comment(models.Model):
 		result =  Comment.objects.filter(guid=gid)
 		return result
 
-	def save(self):
-		if not self.guid:
-			self.guid=uuid.uuid4()
-			super(Comment, self).save()
+
 	      
 class Room(models.Model):
 	room_id = models.IntegerField(primary_key=True)
@@ -36,7 +39,11 @@ class Room(models.Model):
     	type_id = models.ForeignKey('Room_Type')
     	description = models.CharField(max_length=200)
     	rating = models.IntegerField(default=0, null=True)
-    	#guid    
+	guid = models.CharField(max_length=36)
+	def save(self):
+		if not self.guid:
+			self.guid=uuid.uuid4()
+			super(Room, self).save()
 
     	def __unicode__(self):
         	return "ID#" + str(self.room_id)+ ", floor_id=" + str(self.floor_id) + ", type_id=" + str(self.type_id)
