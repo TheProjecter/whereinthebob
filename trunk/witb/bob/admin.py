a9ba7958e22e15f0fede99f9eecc5a75125cd9a5
urlea.cristian@gmail.com <urlea.cristian@gmail.com@2daf306e-0b05-1ee2-9bcf-86be08f4e8b8>
2012-02-21 18:35:02 +0000

    comments admin

--- trunk/witb/bob/admin.py
+++ trunk/witb/bob/admin.py
@@ -1,8 +1,14 @@
 from bob.models import Floor
+from bob.models import Comment
 from django.contrib import admin
 
 class FloorAdmin(admin.ModelAdmin):
 	readonly_fields = ['date_created', 'date_updated', 'rating']
 	fields = ['floor_id', 'description', 'level', 'date_created', 'date_updated', 'rating']
 
+class CommentAdmin(admin.ModelAdmin):
+	readonly_fields = ['date_created', 'date_updated']
+	fields = ['comment_id' , 'comment' , 'guid' , 'date_created','date_updated']
+	
 admin.site.register(Floor, FloorAdmin)
+admin.site.register( Comment, CommentAdmin)
