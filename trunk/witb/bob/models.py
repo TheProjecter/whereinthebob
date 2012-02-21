1f68531b204264343ce527afa712a4069d8a800a
andrey.palade@gmail.com <andrey.palade@gmail.com@2daf306e-0b05-1ee2-9bcf-86be08f4e8b8>
2012-02-21 19:40:37 +0000

    added room, room_type and room_name

--- trunk/witb/bob/models.py
+++ trunk/witb/bob/models.py
@@ -20,4 +20,24 @@ class Comment(models.Model):
 	
 	def __unicode__(self):
 		return "ID#" + str(self.comment_id) + ", date_created=" + str(self.date_created) + ", guid=" +str(self.guid)
+      
+class Room(models.Model):
+    room_id = models.IntegerField(primary_key=True)
+    floor_id = models.ForeignKey('Floor');
+    type_id = models.ForeignKey('Room_Type')
+    description = models.CharField(max_length=200)
+    rating = models.IntegerField(default=0, null=True)
+    #guid    
 
+    def __unicode__(self):
+        return "ID#" + str(self.room_id)+ ", floor_id=" + str(self.floor_id) + ", room_name=" + str(self.room_name) + ", type_id=" + str(self.type_id)
+    
+    
+class Room_Type(models.Model):
+    type_id = models.IntegerField(primary_key=True)
+    type_name = models.CharField(max_length=100) 
+
+
+class Room_Name(models.Model):
+    room_id = models.ForeignKey('Room')
+    name = models.CharField(max_length=50);
