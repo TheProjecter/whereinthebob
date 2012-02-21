d8bb1d3a9c3d274795ab09413d9897df0cf478ea
urlea.cristian <urlea.cristian@gmail.com>
2012-02-21 18:29:47 +0000

    added comment model

--- witb/bob/models.py
+++ witb/bob/models.py
@@ -10,3 +10,14 @@ class Floor(models.Model):
 
 	def __unicode__(self):
 		return "ID#" + str(self.floor_id)+ ", date_created=" + str(self.date_created) + ", level=" + str(self.level) + ", rating=" + str(self.rating)
+
+class Comment(models.Model):
+	comment_id = models.IntegerField(primary_key=True)
+	guid = models.CharField(max_length=360)
+	comment = models.CharField(max_length=1024)
+	date_created = models.DateTimeField(editable=False,auto_now_add=True)
+	date_updated = models.DateTimeField(null=True,editable=False, auto_now=True)
+	
+	def __unicode__(self)
+		return "ID#" + str(self.comment_id) + ", date_created=" + str(self.date_created) + ", guid=" +str(self.guid)
+
