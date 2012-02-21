3c1c621f32b333c75e9fa86531137aca066de281
andrey.palade <andrey.palade@gmail.com>
2012-02-21 20:30:14 +0000

    Fried fish

--- witb/bob/admin.py
+++ witb/bob/admin.py
@@ -8,9 +8,13 @@ class FloorAdmin(admin.ModelAdmin):
 class CommentAdmin(admin.ModelAdmin):
 	readonly_fields = ['date_created', 'date_updated']
 	fields = ['comment_id' , 'comment' , 'guid' , 'date_created','date_updated']
+
+class RoomAdmin(admin.ModelAdmin):
+	readonly_fields = ['rating']
+	fields = ['room_id' ,'floor_id', 'type_id', 'description','rating']
 	
 admin.site.register(Floor, FloorAdmin)
-admin.site.register( Comment, CommentAdmin)
-admin.site.register(Room)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Room, RoomAdmin)
 admin.site.register(Room_Name)
 admin.site.register(Room_Type)
