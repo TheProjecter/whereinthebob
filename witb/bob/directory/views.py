15b47306556bd39699aad9b0a2126768835be935
andrei.mustata <andrei.mustata@gmail.com>
2012-02-22 06:02:33 +0000

    added Directory view

--- witb/bob/directory/views.py
+++ witb/bob/directory/views.py
@@ -1,12 +1,16 @@
+from bob.models import Floor
 from django.template import Context, loader
 from django.http import HttpResponse
+from django.shortcuts import get_list_or_404
 
 
 def index(request):
+    floors = get_list_or_404(Floor)
     t = loader.get_template('listing.html')
     c = Context({
         'title': 'Directory of rooms and floors',
-        'home' : False
+        'home' : False,
+        'floors' : floors
     })
     return HttpResponse(t.render(c))
     
