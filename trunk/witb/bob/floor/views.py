da9615904d6c21eaebc4a7546ab370b97dcdc57f
andrei.mustata@gmail.com <andrei.mustata@gmail.com@2daf306e-0b05-1ee2-9bcf-86be08f4e8b8>
2012-02-22 05:34:26 +0000

    added the floor view; made some changes in the Room template

--- trunk/witb/bob/floor/views.py
+++ trunk/witb/bob/floor/views.py
@@ -1,18 +1,18 @@
-from bob.models import Room
+from bob.models import Floor
 from django.template import Context, loader
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
 
 def index(request, id):
-	room = get_object_or_404(Room, room_id=id)
-	comments = room.comments.model.objects.all()
+	floor = get_object_or_404(Floor, floor_id=id)
+	comments = floor.comments.model.objects.all()
 
 	t = loader.get_template('room.html')
 	c = Context({
-		'title': 'Room',
+		'title': 'Floor',
 		'home' : False,
-		'room' : room,
+		'room' : floor,
 		'comments' : comments,
 		'id' : id 	#kept only for the placeholder image;
 	})
