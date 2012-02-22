31e55ea9445c2521ddb3113c7ee82926ab1ac6aa
andrei.mustata@gmail.com <andrei.mustata@gmail.com@2daf306e-0b05-1ee2-9bcf-86be08f4e8b8>
2012-02-22 05:21:51 +0000

    urls: changed /room to accept an integer and pass it as id; room.views: gets a models.Room object or throws a 404; template/room.html: updated to show the Floor ID, Room ID and comments (only shows ALL the comments in the DB, not just for that room)

--- trunk/witb/bob/room/views.py
+++ trunk/witb/bob/room/views.py
@@ -1,12 +1,19 @@
+from bob.models import Room
 from django.template import Context, loader
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 
-def index(request):
-    t = loader.get_template('room.html')
-    c = Context({
-        'title': 'Room',
-        'home' : False,
-    })
-    return HttpResponse(t.render(c))
+def index(request, id):
+	room = get_object_or_404(Room, room_id=id)
+	comments = room.comments.model.objects.all()
+
+	t = loader.get_template('room.html')
+	c = Context({
+		'title': 'Room',
+		'home' : False,
+		'room' : room,
+		'comments' : comments
+	})
+	return HttpResponse(t.render(c))
     
