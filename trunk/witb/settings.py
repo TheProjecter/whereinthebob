c13599741491b05fd8ae7a4d648bfb0d9b21f9b5
andrei.mustata@gmail.com <andrei.mustata@gmail.com@2daf306e-0b05-1ee2-9bcf-86be08f4e8b8>
2012-02-24 03:42:45 +0000

    fixing my no-no's

--- trunk/witb/settings.py
+++ trunk/witb/settings.py
@@ -1,6 +1,6 @@
 # Django settings for witb project.
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/users/level3/0907390m/workspace/school/dim3/whereinthebob/witb/bob/static'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
