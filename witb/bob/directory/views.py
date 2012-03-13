6cfdee32f003571ef7a18ab46ad97e4951ab14cd
andrei.mustata <andrei.mustata@2daf306e-0b05-1ee2-9bcf-86be08f4e8b8>
2012-03-13 03:16:34 +0000

    Directory: fixed listing template and directory view, now showing all the rooms

--- witb/bob/directory/views.py
+++ witb/bob/directory/views.py
@@ -1,4 +1,4 @@
-from bob.models import Floor
+from bob.models import Floor, Room
 from django.template import Context, loader
 from django.http import HttpResponse
 from django.shortcuts import get_list_or_404
@@ -6,11 +6,13 @@ from django.shortcuts import get_list_or_404
 
 def index(request):
     floors = get_list_or_404(Floor)
+    rooms = get_list_or_404(Room)
     t = loader.get_template('listing.html')
     c = Context({
         'title': 'Directory of rooms and floors',
         'home' : False,
-        'floors' : floors
+        'floors' : floors,
+        'rooms' : rooms
     })
     return HttpResponse(t.render(c))
     
